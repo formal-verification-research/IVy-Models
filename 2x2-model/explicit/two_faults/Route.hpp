5e97f8949d5218798dace1a0766a3a11fcbe07c7
Landon Taylor <56940292+landonjefftaylor@users.noreply.github.com>
2020-05-22 09:26:02 -0600

    Folder Update

--- 2x2-model/explicit/two_faults/Route.hpp
+++
@@ -1,3 +0,0 @@
-
-std::string route_default(int cur_x, int cur_y, int dest_x, int dest_y);
-void route(int cur_x, int cur_y, int dest_x, int dest_y, int faulty_x1, int faulty_y1, char faulty_dir1, int faulty_x2, int faulty_y2, char faulty_dir2);
--- 2x2-model/explicit/two_faults/Utils.hpp
+++
@@ -1,4 +0,0 @@
-#include <string>
-
-char direction(const char c);
-std::string longDirection(const char c);
